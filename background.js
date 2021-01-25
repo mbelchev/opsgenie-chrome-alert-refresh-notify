67d2b0c03c667d230aa2c15949c7f69e009ea42a
mbelchev <marianbelchev@gmail.com>
2021-01-25 22:55:20 +0200

    Adding background runtime onmessage listener

--- background.js
+++ background.js
@@ -1,3 +1,17 @@
 chrome.browserAction.onClicked.addListener(() =>
-	chrome.tabs.executeScript(null, { file: 'alertautorefresh.js' })
+  chrome.tabs.executeScript(null, { file: 'alertautorefresh.js' })
 );
+
+const raiseNotification = (request) => {
+  return chrome.notifications.create('', {
+    type: 'basic',
+    iconUrl: 'assets/128x128.png',
+    title: 'Opsgenie Alert',
+    message: request.body,
+    contextMessage: request.context
+  })
+}
+
+chrome.runtime.onMessage.addListener((request, sender, callback) => {
+  callback(raiseNotification(request))
+});
